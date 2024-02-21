f5265ae27490f7fb32646063e90b1b069e4208d3
excalibrium <103365858+excalibrium@users.noreply.github.com>
2024-02-21 23:49:04 +0300

    Initial commit

---
+++ Objects/button.gd
@@ -0,0 +1,35 @@
+extends Node3D
+
+@export var door : Door
+@export var door2 : Door
+#@onready var Ray = $RayCast3D
+@onready var Area = $Area3D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# This function is called when the area enters another body
+
+
+# Replace "Player" with the actual class name of your player objec
+func _input(event):
+	if event.is_action_pressed("interact"):
+		var overlapping_bodies = Area.get_overlapping_bodies()
+	
+		for A in overlapping_bodies:
+			if A is _Player:  # Replace "Player" with the actual class name of your player object
+				door.toggle()
+				door2.toggle()
+				break  # Exit the loop if you've found the player
+#		if Ray.is_colliding():
+#			var collider = Ray.get_collider()
+#			if collider is _Player:
+#				print("RAY colliding too")
+
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
